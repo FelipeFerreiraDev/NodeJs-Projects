dd5711d23574fd5c3fbbf297d9e1600f1df25d0e
Felipe H. C. Ferreira <48157305+FelipeFerreiraDev@users.noreply.github.com>
2022-10-25 18:40:54 -0300

    Delete Nest/restaurante directory

--- Nest/restaurante/src/modules/restaurante/dto/update-restaurante.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateRestauranteDto } from './create-restaurante.dto';
-
-export class UpdateRestauranteDto extends PartialType(CreateRestauranteDto) {
-  name?: string;
-  endereco?: string;
-  horario?: string;
-  password?: string;
-}
