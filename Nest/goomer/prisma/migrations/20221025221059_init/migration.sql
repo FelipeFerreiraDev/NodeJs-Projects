1b42d4e65d940e7f5205a1d50567f06a99f6fc28
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-10-25 19:44:41 -0300

    chore: executando migrations

---
+++ Nest/goomer/prisma/migrations/20221025221059_init/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "Restaurant" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "address" TEXT,
+    "operation" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "category" TEXT NOT NULL,
+    "content" TEXT,
+    "promotionalPrice" REAL,
+    "date" DATETIME,
+    "restaurantId" TEXT NOT NULL,
+    CONSTRAINT "Product_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Restaurant_email_key" ON "Restaurant"("email");
